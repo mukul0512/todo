5505f64b358318876f319df62c9d02d7327da3e6
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-05-28 16:03:38 +0530

    implementing redux concepts using simple Todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todo';
 
 function App() {
 
   return (
     <>
-
+      <AddTodo />
+      <Todos />
     </>
   )
 }
